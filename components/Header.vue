0e4dfc16c9ae24f864c136e9868ea8c0bce83e77
sheymantech <olaniyansheyi1704@gmail.com>
2024-07-07 23:46:32 +0100

    new feature

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,13 @@
 <script setup>
 import logoWhite from "~/public/img/logo-white.png";
 import logoBlack from "~/public/img/logo-black.png";
+import menu from "~/public/img/menu.svg";
 </script>
 
 <template>
   <div class="w-full flex justify-between items-center text-white px-8">
     <img class="w-[30px] hidden sm:block" :src="logoBlack" alt="" />
-    <img :src="logoWhite" class="w-[35px]" alt="" />
+    <img :src="logoWhite" class="w-[35px] sm:hidden" alt="" />
     <nav
       class="hidden flex items-center justify-center gap-x-6 lg:gap-x-10 tracking-wide font-semibold text-xs"
     >
@@ -17,8 +18,8 @@ import logoBlack from "~/public/img/logo-black.png";
         Contact Me
       </button>
     </nav>
-    <div>
-      <img src="" alt="" />
+    <div class="cursor-pointer">
+      <img :src="menu" alt="" />
     </div>
   </div>
 </template>
