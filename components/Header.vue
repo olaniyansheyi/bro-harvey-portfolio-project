ceb24cf62a01afb6a640ccef6194175dec6b333d
sheymantech <olaniyansheyi1704@gmail.com>
2024-07-08 00:44:31 +0100

    new feature

--- components/Header.vue
+++ components/Header.vue
@@ -2,24 +2,48 @@
 import logoWhite from "~/public/img/logo-white.png";
 import logoBlack from "~/public/img/logo-black.png";
 import menu from "~/public/img/menu.svg";
+import cancel from "~/public/img/cancel.png";
+
+const openMenu = ref(false);
+
+const handleToggleMenu = () => {
+  openMenu.value = !openMenu.value;
+};
 </script>
 
 <template>
-  <div class="w-full flex justify-between items-center text-white px-8">
-    <img class="w-[30px] hidden sm:block" :src="logoBlack" alt="" />
-    <img :src="logoWhite" class="w-[35px] sm:hidden" alt="" />
-    <nav
-      class="hidden flex items-center justify-center gap-x-6 lg:gap-x-10 tracking-wide font-semibold text-xs"
+  <div
+    class="sm:static absolute top-[5%] left-0 right-0 z-[6] sm:bg-transparent bg-black"
+  >
+    <div
+      class="w-full flex justify-between items-start text-white px-8 pb-4 border-white relative"
+      :class="openMenu ? 'border-b-[1px] h-[15rem] items-start' : ''"
     >
-      <Nuxt>About me</Nuxt>
-      <Nuxt>Skills</Nuxt>
-      <Nuxt>Portfolio</Nuxt>
-      <button class="text-black rounded-full p-2 bg-white text-xs">
-        Contact Me
-      </button>
-    </nav>
-    <div class="cursor-pointer">
-      <img :src="menu" alt="" />
+      <img class="w-[30px] hidden sm:block" :src="logoBlack" alt="" />
+      <img :src="logoWhite" class="w-[35px] sm:hidden" alt="" />
+      <nav
+        :class="
+          openMenu
+            ? ' flex flex-col absolute bottom-0 left-0 right-0 justify-start items-center h-auto gap-y-5 border-white pt-6 border-t-[1px]'
+            : 'hidden sm:flex items-center justify-center gap-x-6 lg:gap-x-10 tracking-wide font-semibold text-xs'
+        "
+      >
+        <Nuxt>About me</Nuxt>
+        <Nuxt>Skills</Nuxt>
+        <Nuxt>Portfolio</Nuxt>
+        <button
+          class="text-black rounded-full p-2 bg-white text-xs"
+          :class="
+            openMenu ? 'w-full rounded-none shadow-lg shadow-black/60 ' : ''
+          "
+        >
+          Contact Me
+        </button>
+      </nav>
+      <div @click="handleToggleMenu" class="cursor-pointer sm:hidden">
+        <img :class="openMenu ? 'hidden' : 'block'" :src="menu" alt="" />
+        <img :class="openMenu ? 'block' : 'hidden'" :src="cancel" alt="" />
+      </div>
     </div>
   </div>
 </template>
